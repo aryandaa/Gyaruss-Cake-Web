e3d95f90ccb77d3d6f7d785fb711869fc8d4069c
R3X <114203619+aryandaa@users.noreply.github.com>
2025-11-09 18:11:34 +0800

    `Added admin index and login pages with database connection script`

---
+++ admin/login.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Asset/fonts/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Style -->
+    <link rel="stylesheet" href="../Asset/css/style.css">
+    <link rel="stylesheet" href="../Asset/css/custombootstrap.css">
+    <title>Login Admin</title>
+  </head>
+  <body>
+  
+  <div class="content">
+    <div class="container">
+      <div class="row">
+        
+        <div class="col-md-6 contents">
+          <div class="row justify-content-center">
+
+            <div class="col-md-6">
+          <img src="../Asset/images/logo gyarus.png" alt="Image" class="img-fluid">
+            </div>
+            
+            <div class="col-md-8">
+
+            
+              <div class="mb-4">
+              <h3>Login Admin</h3>
+              </div>
+            <form action="#" method="post">
+              <div class="form-group first">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username">
+
+              </div>
+              <div class="form-group last mb-4">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password">
+                
+              </div>
+
+              <input type="submit" value="Log In" class="btn btn-block btn-success">
+              
+            </form>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <img src="../Asset/images/Ilustrator.png" alt="Image" class="img-fluid">
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
